397557a0e1485aabc468674172aa6525edf3ceb1
DieterJoubert <dieterjoubert@gmail.com>
2022-12-26 10:55:49 -0700

    day_5

--- 2022/day_5.py
+++ 2022/day_5.py
@@ -1,20 +1,69 @@
+from functools import reduce
+
 DATA_PATH = './data/input_5.txt'
 
-def get_data():
-   stacks = {}
-   instructions = []
+def get_lines():
    with open(DATA_PATH) as f:
       lines = f.read().splitlines()
-      for line in lines:
-         print(line)
-         if not line:
-            break
+      idx = lines.index('')
+      return (lines[:idx], lines[idx+1:])
+
+def get_initial_stacks(lines):
+   data = {}
+   stacks = lines[0]
+   crates = lines[1:]
+
+   for i in range(len(stacks)):
+      curr_char = stacks[i]
+      if curr_char.isdigit():
+         curr_stack = int(curr_char)
+         data[curr_stack] = []
+         for j in range(len(crates)):
+            curr_crate = crates[j][i]
+            if curr_crate.isalpha():
+               data[curr_stack].append(curr_crate)
+   
    return data
 
+def get_instructions_from_lines(lines):
+   data = []
+   for line in lines:
+      curr_instruction = []
+      for segment in line.split():
+         if segment.isdigit():
+            curr_instruction.append(segment)
+      data.append(tuple(map(int, curr_instruction)))
+   return data
+
+def get_data():
+   stack_lines, instruction_lines = get_lines()
+   stacks = get_initial_stacks(stack_lines[::-1])
+   instructions = get_instructions_from_lines(instruction_lines)
+   return (stacks, instructions)
+
+def perform_instruction(stacks, instruction):
+   num, from_crate, to_crate = instruction
+
+   for _ in range(num):
+      item = stacks[from_crate].pop()
+      stacks[to_crate].append(item)
+
+   return stacks
+
+def perform_instructions(stacks, instructions):
+   return reduce(lambda s, inst: perform_instruction(s, inst), instructions, stacks)
+
+def get_soln(final_stacks):
+   soln = ''
+   for k in sorted(final_stacks.keys()):
+      soln += final_stacks[k][-1]
+   return soln
 
 def main():
-   data = get_data()
-
+   stacks, instructions = get_data()
+   final_stacks = perform_instructions(stacks, instructions)
+   soln = get_soln(final_stacks)
+   print(soln)
 
 if __name__ == "__main__":
    main()
--- 2022/day_8.py
+++ 2022/day_8.py
@@ -1,5 +1,24 @@
 DATA_PATH = './data/input_8.txt'
 
+class Highest(object):
+   def __init__(self):
+      self.left = 0
+      self.right = 0
+      self.top = 0
+      self.bottom = 0
+
+   def set_left(self, new_left):
+      self.left = max(self.left, new_left)
+
+   def set_right(self, new_right):
+      self.right = max(self.right, new_right)
+
+   def set_top(self, new_top):
+      self.top = max(self.top, new_top)
+
+   def set_bottom(self, new_bottom):
+      self.bottom = max(self.bottom, new_bottom)
+
 def get_data():
    grid = []
    with open(DATA_PATH) as f:
@@ -8,8 +27,20 @@ def get_data():
          grid.append(list(map(int, line)))
    return grid
       
+def initialize_lookup(grid):
+   lookup = {}
+   for i in range(len(grid)):
+      for j in range(len(grid[0])):
+         lookup[(i, j)] = Highest()
+   return lookup
+
+def solve(grid):
+   lookup = initialize_lookup(grid)
+
+
 def main():
    data = get_data()
+   soln = solve(data)
    print(data)
 
 if __name__ == "__main__":
