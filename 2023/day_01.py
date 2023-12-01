3a64597076025c36c8868aaab3039f94f728c8a8
DieterJoubert <dieterjoubert@gmail.com>
2023-12-01 01:26:14 -0800

    day 1

--- 2023/day_01.py
+++ 2023/day_01.py
@@ -1,21 +1,69 @@
-#todo
+DATA_PATH = './data/input_01.txt'
 
 def get_data():
-    data = [[]]
-    with open('./data/input_1.txt') as f:
+    with open(DATA_PATH) as f:
         lines = f.read().splitlines()
-        for line in lines:
-          if not line:
-            data.append([])
-          else:
-            data[-1].append(int(line))
-    return data
+        return lines
 
+# def get_line_calibration_value(line):
+#    first = next(x for x in line if x.isnumeric())
+#    last = next(x for x in line[::-1] if x.isnumeric())
+#    return int(first + last)
+
+int_map = {
+   'one': 1,
+   'two': 2, 
+   'three': 3, 
+   'four': 4, 
+   'five': 5, 
+   'six': 6, 
+   'seven': 7, 
+   'eight': 8, 
+   'nine': 9
+}
+
+def check_for_hidden_int(segment):
+   if segment[:3] in int_map:
+      return int_map[segment[:3]]
+   if segment[:4] in int_map:
+      return int_map[segment[:4]]
+   if segment[:5] in int_map:
+      return int_map[segment[:5]]
+   return None 
+   
+def get_foobar(line):
+  first, last = None, None
+  
+  for idx in range(len(line)):
+      char = line[idx]
+      if char.isnumeric():
+         first = int(char)
+         break
+      hidden_int = check_for_hidden_int(line[idx:idx+5])
+      if hidden_int:
+         first = hidden_int
+         break
+      
+  for idx in range(len(line)-1, -1, -1):
+      char = line[idx]
+      if char.isnumeric():
+         last = int(char)
+         break
+      hidden_int = check_for_hidden_int(line[idx:idx+5])
+      if hidden_int:
+         last = hidden_int
+         break
+  
+  return int(str(first) + str(last))
+      
 def main():
   data = get_data()
 
-  sums = [sum(x) for x in data]
-  print(max(sums))
+  calibration_values = [get_foobar(x) for x in data]
+
+  #print(calibration_values)
+  print(sum(calibration_values))
+
 
 if __name__ == "__main__":
   main()
