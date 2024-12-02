7520c5522c1dad9722d2a8cae399fbc4a81b773a
DieterJoubert <dieterjoubert@gmail.com>
2024-12-02 09:08:35 -0500

    day two, part two

--- 2024/day_02.py
+++ 2024/day_02.py
@@ -11,60 +11,60 @@ def get_data(is_test):
 		data.append(list(map(int, line.split())))
 	return data
 
+def is_safe(level):
+	differences = set()
+
+	for i in range(len(level)-1):
+		a, b = level[i], level[i+1]
+		differences.add(b-a)
+
+	if 0 in differences:
+		return False
+	
+	max_val = max(differences)
+	min_val = min(differences)
+
+	if min_val < 0 and max_val > 0:
+		return False
+	if min_val < -3 or max_val > 3:
+		return False
+
+	return True
+
 def part_one(data):
-	def is_safe(level):
-		differences = set()
-
-		for i in range(len(level)-1):
-			a, b = level[i], level[i+1]
-			differences.add(b-a)
-
-		if 0 in differences:
-			return False
-		
-		max_val = max(differences)
-		min_val = min(differences)
-
-		if min_val < 0 and max_val > 0:
-			return False
-		if min_val < -3 or max_val > 3:
-			return False
-
-		return True
-
 	return sum([is_safe(level) for level in data])
+
+def get_subarrays(arr):
+	result = []
+	for i in range(len(arr)):
+		subarray = arr[:i] + arr[i+1:]
+		result.append(subarray)
+	return result
 
 def part_two(data):
-	def is_safe(level):
-		differences = []
-
-		for i in range(len(level)-1):
-			a, b = level[i], level[i+1]
-			differences.append(b-a)
-
-		if 0 in differences:
-			return False
+	def is_safe_with_mod(level):
+		if is_safe(level):
+			return True
 		
-		max_val = max(differences)
-		min_val = min(differences)
+		sub_arrays = get_subarrays(level)
 
-		if min_val < 0 and max_val > 0:
-			return False
-		if min_val < -3 or max_val > 3:
-			return False
+		for array in sub_arrays:
+			if is_safe(array):
+				return True
+			
+		return False
 
-		return True
-
-	return sum([is_safe(level) for level in data])
+	return sum([is_safe_with_mod(level) for level in data])
 
 def main(is_test):
 	data = get_data(is_test)
+
 	print('one: ', part_one(data))
+	print('two: ', part_two(data))
 
 	if not is_test:
 		assert part_one(data) == 236
-
-
+		assert part_two(data) == 308
 
 if __name__ == "__main__":
 	main(len(sys.argv) > 1 and sys.argv[1].lower() == '--test')
